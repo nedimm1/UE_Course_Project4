61403ea34954ab26a896df8bba8d43dea5760f95
github_username <nedimmuminovic61@gmail.com>
2022-11-16 14:14:59 +0100

    Making the Enemy_Turret point in the direction of the Tank

--- Source/ToonTanks/Enemy_Turret.cpp
+++ Source/ToonTanks/Enemy_Turret.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Enemy_Turret.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
 
+
+void AEnemy_Turret::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Find the distance to the Tank
+    if (Tank)
+    {
+        float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+
+        // Check to see if the Tank is in range
+        if (Distance <= FireRange)
+        {
+            // If in range, rotate turret toward Tank
+            RotateTurret(Tank->GetActorLocation());
+        }
+    }
+}
+
+void AEnemy_Turret::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
--- Source/ToonTanks/Enemy_Turret.h
+++ Source/ToonTanks/Enemy_Turret.h
@@ -13,5 +13,22 @@ UCLASS()
 class TOONTANKS_API AEnemy_Turret : public ABasePawn
 {
 	GENERATED_BODY()
-	
+
+
+    public:
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 500.f;
 };
+
+	
