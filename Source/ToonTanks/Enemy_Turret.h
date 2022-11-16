322eb3aec0935a17d0194778aac19b257ed4813f
github_username <nedimmuminovic61@gmail.com>
2022-11-16 13:22:47 +0100

    Creating a new class based of of BasePawn

---
+++ Source/ToonTanks/Enemy_Turret.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Enemy_Turret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API AEnemy_Turret : public ABasePawn
+{
+	GENERATED_BODY()
+	
+};
