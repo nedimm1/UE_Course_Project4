321e396a0b3ee17f40ca997223f3779bfc6b9504
github_username <nedimmuminovic61@gmail.com>
2022-11-29 13:46:07 +0100

    Experimenting with hit events

--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -28,9 +28,15 @@ private:
 
 
 
+    UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+    
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	
+
 
 };
 
