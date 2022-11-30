6a38cd08d2db8e20b135b342080b578115d14c9a
github_username <nedimmuminovic61@gmail.com>
2022-11-30 13:27:12 +0100

    creating a Player Controler class

---
+++ Source/ToonTanks/ToonTanks_Controler.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TTsControler.generated.h"
+
+
+UCLASS()
+class TOONTANKS_API ATTsControler : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
